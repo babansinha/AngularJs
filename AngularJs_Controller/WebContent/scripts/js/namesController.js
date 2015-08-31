11c0290a3ecfe7cb9890c00ac80c919d13f0b1dd
babansinha <sinha.baban@gmail.com>
2015-08-31 22:50:10 +0530

    Adding controller concepts

---
+++ AngularJs_Controller/WebContent/scripts/js/namesController.js
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+app.controller('namesCtrl', function($scope) {
+    $scope.names = [
+        {name:'Baban',country:'Sinha'},
+        {name:'Bhanu',country:'Prakash'},
+        {name:'Ahilya',country:'Sinha'}
+    ];
+});
